9f4dfa15ea84298a433e46f2730151eba37485fe
Souravdey777 <piyush.kolkata@gmail.com>
2020-05-15 00:07:41 +0530

    check 3

--- index.js
+++ index.js
@@ -1,189 +1,47 @@
-// dependencies
-'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
-const http = require('https');
-var unirest = require("unirest");
-let errorResposne = {
-    results: []
-};
-var port = process.env.PORT || 8080;
-// create serve and configure it.
-const server = express();
-const { MovieDb } = require('moviedb-promise')
-const moviedb = new MovieDb('6ed9d94bd9ea7880be31f354d69cc925')
+"use strict";
 
-server.use(bodyParser.json());
-server.post('/getMovies', function (request, response) {
+const express = require("express");
+const bodyParser = require("body-parser");
 
-    //if(request.queryResult.parameters['top-rated']) {
-    // Using just the Promise
-    moviedb.movieTopRated({}).then(res => {
-        //console.log(res)
-        if (res.results.length > 0) {
-            let result = res.results;
-            let output = '';
-            for (let i = 0; i < result.length; i++) {
-                output += result[i].title;
-                output += "\n"
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/echo", function(req, res) {
+  var speech =
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.echoText
+      ? req.body.queryResult.parameters.echoText
+      : "Seems like some problem. Speak again.";
+  
+  var speechResponse = {
+    google: {
+      expectUserResponse: true,
+      richResponse: {
+        items: [
+          {
+            simpleResponse: {
+              textToSpeech: speech
             }
-            response.setHeader('Content-Type', 'application/json');
-            let res_dialogflow={
-                "payload": {
-                  "google": {
-                    "expectUserResponse": true,
-                    "richResponse": {
-                      "items": [
-                        {
-                          "simpleResponse": {
-                            "textToSpeech": "Choose a item"
-                          }
-                        }
-                      ]
-                    },
-                    "systemIntent": {
-                      "intent": "actions.intent.OPTION",
-                      "data": {
-                        "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                        "listSelect": {
-                          "title": "Hello",
-                          "items": [
-                            {
-                              "optionInfo": {
-                                "key": "first title key"
-                              },
-                              "description": "first description",
-                              "image": {
-                                "url": "/assistant/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-                                "accessibilityText": "first alt"
-                              },
-                              "title": "first title"
-                            },
-                            {
-                              "optionInfo": {
-                                "key": "second"
-                              },
-                              "description": "second description",
-                              "image": {
-                                "url": "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
-                                "accessibilityText": "second alt"
-                              },
-                              "title": "second title"
-                            }
-                          ]
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            response.send(JSON.stringify(res_dialogflow));
-        }
-    }).catch(console.error)
-
-
-    // console.log(request.body.queryResult.parameters)
-    // if(request.queryResult.parameters['top-rated']) {
-    // var req = unirest("GET", "https://api.themoviedb.org/3/movie/top_rated");
-    //     req.query({
-    //         "page": "1",
-    //         "language": "en-US",
-    //         "api_key": API_KEY
-    //     });
-    //     req.send("{}");
-    //     req.end(function(res) {
-    //         if(res.error) {
-    //             response.setHeader('Content-Type', 'application/json');
-    //             response.send(JSON.stringify({
-    //                 "speech" : "Error. Can you try it again ? ",
-    //                 "displayText" : "Error. Can you try it again ? "
-    //             }));
-    //         } else if(res.body.results.length > 0) {
-    //             let result = res.body.results;
-    //             let output = '';
-    //             for(let i = 0; i<result.length;i++) {
-    //                 output += result[i].title;
-    //                 output+="\n"
-    //             }
-    //             response.setHeader('Content-Type', 'application/json');
-    //             response.send(JSON.stringify({
-    //                 "speech" : output,
-    //                 "displayText" : output
-    //             })); 
-    //         }
-    //     });
-    // } else if(request.queryResult.parameters['movie-name']) {
-    //   console.log('popular-movies param found');
-    //     let movie = request.body.result.parameters['movie-name'];
-    //     var req = unirest("GET", "https://api.themoviedb.org/3/search/movie");
-    //         req.query({
-    //             "include_adult": "false",
-    //             "page": "1",
-    //             "query":movie,
-    //             "language": "en-US",
-    //             "api_key": API_KEY
-    //         });
-    //         req.send("{}");
-    //         req.end(function(res) {
-    //             if(res.error) {
-    //                 response.setHeader('Content-Type', 'application/json');
-    //                 response.send(JSON.stringify({
-    //                     "speech" : "Error. Can you try it again ? ",
-    //                     "displayText" : "Error. Can you try it again ? "
-    //                 }));
-    //             } else if(res.body.results.length > 0) {
-    //             let result = res.body.results[0];
-    //             let output = "Average Rating : " + result.vote_average + 
-    //             "\n Plot : " + result.overview + "url" + result.poster_path
-    //                 response.setHeader('Content-Type', 'application/json');
-    //                 response.send(JSON.stringify({
-    //                     "speech" : output,
-    //                     "displayText" : output
-    //                 }));
-    //             } else {
-    //                 response.setHeader('Content-Type', 'application/json');
-    //                 response.send(JSON.stringify({
-    //                     "speech" : "Couldn't find any deatails. :(  ",
-    //                     "displayText" : "Couldn't find any deatails. :(  "
-    //                 }));
-    //             }
-    //         });
-
-    // } else if(request.queryResult.parameters['popular-movies']) {    
-    //     var req = unirest("GET", "https://api.themoviedb.org/3/movie/popular");
-    //         req.query({
-    //             "page": "1",
-    //             "language": "en-US",
-    //             "api_key": API_KEY
-    //         });
-    //         req.send("{}");
-    //         req.end(function(res){
-    //             if(res.error) {
-    //                 response.setHeader('Content-Type', 'application/json');
-    //                 response.send(JSON.stringify({
-    //                     "speech" : "Error. Can you try it again ? ",
-    //                     "displayText" : "Error. Can you try it again ? "
-    //                 }));
-    //             } else {
-    //                 let result = res.body.results;
-    //                 let output = '';
-    //                 for(let i = 0; i < result.length;i++) {
-    //                     output += result[i].title;
-    //                     output+="\n"
-    //                 }
-    //                 response.setHeader('Content-Type', 'application/json');
-    //                 response.send(JSON.stringify({
-    //                     "speech" : output,
-    //                     "displayText" : output
-    //                 })); 
-    //             }
-    //         });
-    //}
-});
-server.get('/getName', function (req, res) {
-    res.send('Sourav Dey');
-});
-
-server.listen(port, function () {
-    console.log("Server is up and running...");
+          }
+        ]
+      }
+    }
+  };
+  
+  return res.json({
+    payload: speechResponse,
+    //data: speechResponse,
+    fulfillmentText: speech,
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
 });
